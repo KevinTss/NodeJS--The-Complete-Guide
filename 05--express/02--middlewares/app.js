f4491ee9a294f78009c0eb20f34ed406ed63b5f8
Kevin Tassi <kevin@fishtripr.com>
2018-11-29 18:37:22 +0100

    add example for express & middleware

---
+++ 05--express/02--middlewares/app.js
@@ -0,0 +1,13 @@
+/**
+ * Instal expressjs
+*/
+
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen(3000);
