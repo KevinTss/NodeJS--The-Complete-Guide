b235c7fd2af8f0d10142311b2563548cac3a45a8
Kevin Tassi <kevin@fishtripr.com>
2018-12-02 17:45:34 +0100

    add routing example

--- 05--express/01--install-express/app.js
+++ 05--express/01--install-express/app.js
@@ -1,5 +1,5 @@
 /**
- * Middleware
+ * Instal expressjs
 */
 
 const http = require('http');
@@ -7,17 +7,6 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-
-// This function will be executed for each request to the server
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  // if you execute next, you allowed to passe to the next middleware and
-  // continue the process
-  next();
-})
-app.use((req, res, next) => {
-  console.log('In another middleware');
-})
 
 const server = http.createServer(app);
 
--- 05--express/02--middlewares/app.js
+++ 05--express/02--middlewares/app.js
@@ -1,13 +1,29 @@
 /**
- * Instal expressjs
+ * Middleware
 */
 
-const http = require('http');
+// No need this line cause it's include in express
+// const http = require('http');
 
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+// This function will be executed for each request to the server
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  // if you execute next, you allowed to passe to the next middleware and
+  // continue the process
+  next();
+})
+app.use((req, res, next) => {
+  console.log('In another middleware');
+})
 
-server.listen(3000);
+// We can also replace the creation serve
+// Express do it for us
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+app.listen(3000);
